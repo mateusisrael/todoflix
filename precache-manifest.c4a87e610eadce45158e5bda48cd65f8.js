df58757235a4b6b5eb83db10b4d320ef9e651fe5
mateusisrael <mateuss.i.fernandes@gmail.com>
2020-01-20 21:30:22 -0300

    tentando

---
+++ precache-manifest.c4a87e610eadce45158e5bda48cd65f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304ab4a7613f8d3083efc8ae3e54d419",
+    "url": "/todoflix/index.html"
+  },
+  {
+    "revision": "9c546aca718d6a599cbb",
+    "url": "/todoflix/static/css/main.b3ac719b.chunk.css"
+  },
+  {
+    "revision": "eb7963dcc32f03a5df52",
+    "url": "/todoflix/static/js/2.8b582340.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todoflix/static/js/2.8b582340.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c546aca718d6a599cbb",
+    "url": "/todoflix/static/js/main.90c0e222.chunk.js"
+  },
+  {
+    "revision": "f3e88ff2b5073e18b6c9",
+    "url": "/todoflix/static/js/runtime-main.68288828.js"
+  },
+  {
+    "revision": "91acda5f8504b210788d79b671252d6c",
+    "url": "/todoflix/static/media/capitc3a3o-fantc3a1stico1.91acda5f.png"
+  },
+  {
+    "revision": "ecbf82388f0e1e077546e4f8d0acefd5",
+    "url": "/todoflix/static/media/claquete.ecbf8238.svg"
+  },
+  {
+    "revision": "266c9d507760d0173cf3dfba7251f50b",
+    "url": "/todoflix/static/media/estrela-cinza.266c9d50.svg"
+  },
+  {
+    "revision": "f8518c029ccc17eab0577334a75083ee",
+    "url": "/todoflix/static/media/estrela.f8518c02.svg"
+  },
+  {
+    "revision": "409ce39aff2be5bc94f3be393ad67e31",
+    "url": "/todoflix/static/media/images1.409ce39a.png"
+  },
+  {
+    "revision": "4ad68bb426e60ae8d3c78d0621ee9a7f",
+    "url": "/todoflix/static/media/movie-camera-show-Cinema-Illustration.4ad68bb4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42b29175bef6b84c05d211b9353bd6a6.js"
+  "/todoflix/precache-manifest.c4a87e610eadce45158e5bda48cd65f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todoflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
